4399b7a28fb810117bc8a9337fb7106f6b2b0b2b
FouadHellal <113594352+FouadHellal@users.noreply.github.com>
2024-05-10 17:45:08 +0100

    Commit

--- zyrawebsite/settings.py
+++ zyrawebsite/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
 import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'zyraapp/static'),
